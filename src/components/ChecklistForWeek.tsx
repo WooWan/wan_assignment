387b13749224bb0bb9763bf78bcda54ddb80711e
WooWan <woohobi@gmail.com>
2023-12-02 17:25:34 +0900

    update: checklist slide 애니메이션

--- src/components/ChecklistForWeek.tsx
+++ src/components/ChecklistForWeek.tsx
@@ -1,9 +1,19 @@
-import React, {useCallback, useMemo} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 import {CheckItem} from '../types/checklist';
 import {FlatList, ListRenderItem} from 'react-native';
 import CheckListItem from './CheckListItem';
 import NoCheckList from './NoCheckList';
 import ProgressBar from './ProgressBar';
+import Animated, {
+  EntryAnimationsValues,
+  ExitAnimationsValues,
+  SlideInLeft,
+  SlideInRight,
+  SlideOutLeft,
+  SlideOutRight,
+  useSharedValue,
+} from 'react-native-reanimated';
+import {useDebounce} from '../utils/useDebounce';
 
 type Props = {
   checklist: CheckItem[];
@@ -12,6 +22,12 @@ type Props = {
   handleDeleteChecklist: (id: string) => void;
   toggleCheckList: (id: string) => void;
 };
+
+const slideOutLeftAnimation = new SlideOutLeft().build();
+const slideOutRightAnimation = new SlideOutRight().build();
+const slideInLeftAnimation = new SlideInLeft().build();
+const slideInRightAnimation = new SlideInRight().build();
+
 function ChecklistForWeek({
   checklist,
   week,
@@ -19,14 +35,21 @@ function ChecklistForWeek({
   handleDeleteChecklist,
   toggleCheckList,
 }: Props) {
+  const debouncedWeek = useDebounce(week, 500);
   const checklistForWeek = useMemo(
-    () => checklist?.filter(item => item.weekNumber === week),
-    [checklist, week],
+    () => checklist?.filter(item => item.weekNumber === debouncedWeek),
+    [checklist, debouncedWeek],
   );
   const completedCount = useMemo(
     () => checklistForWeek?.filter(item => item.isCompleted).length,
     [checklistForWeek],
   );
+  const [previousWeek, setPreviousWeek] = useState(week);
+  const exitDirection = useSharedValue('left');
+
+  useEffect(() => {
+    setPreviousWeek(debouncedWeek);
+  }, [debouncedWeek]);
 
   const renderCheckList: ListRenderItem<CheckItem> = useCallback(
     ({item, index}) => {
@@ -43,11 +66,35 @@ function ChecklistForWeek({
     [isEditMode, toggleCheckList, handleDeleteChecklist],
   );
 
+  const CustomExitingAnimation = (values: ExitAnimationsValues) => {
+    'worklet';
+
+    return exitDirection.value === 'left'
+      ? slideOutLeftAnimation(values)
+      : slideOutRightAnimation(values);
+  };
+  const CustomEnteringAnimation = (values: EntryAnimationsValues) => {
+    'worklet';
+
+    return exitDirection.value === 'left'
+      ? slideInRightAnimation(values)
+      : slideInLeftAnimation(values);
+  };
+
+  useEffect(() => {
+    if (week > previousWeek) {
+      exitDirection.value = 'left';
+    } else {
+      exitDirection.value = 'right';
+    }
+  }, [week, previousWeek, exitDirection]);
+
   return (
-    <>
-      {checklistForWeek?.length === 0 ? (
-        <NoCheckList />
-      ) : (
+    <Animated.View
+      key={debouncedWeek}
+      entering={CustomEnteringAnimation}
+      exiting={CustomExitingAnimation}>
+      {checklistForWeek?.length !== 0 ? (
         <>
           <ProgressBar
             total={checklistForWeek.length}
@@ -59,8 +106,10 @@ function ChecklistForWeek({
             keyExtractor={item => item.id}
           />
         </>
+      ) : (
+        <NoCheckList />
       )}
-    </>
+    </Animated.View>
   );
 }
 
